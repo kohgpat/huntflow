45cb3b2623a111240b97d2619b9d6f2b1827699d
Nikolay Burlov <kohgpat@gmail.com>
2018-09-05 23:07:47 +0800

    Fix Button formatting

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -6,51 +6,51 @@
 
 <script>
 export default {
-  name: 'Button',
+  name: "Button",
   props: {
     variant: String
   },
   computed: {
     getClass() {
       return {
-        "button": true,
+        button: true,
         "button--primary": this.variant === "primary",
         "button--success": this.variant === "success"
-      }
+      };
     }
   }
-}
+};
 </script>
 
 <style scoped>
-  .button {
-    padding: 9px 15px 8px;
-    font-weight: 700;
-    font-size: 11px;
-    line-height: 100%;
-    text-transform: uppercase;
-    background-color: #fff;
-    border: 1px solid #ccc;
-    border-radius: 3px;
-    color: #38343F;
-  }
+.button {
+  padding: 9px 15px 8px;
+  font-weight: 700;
+  font-size: 11px;
+  line-height: 100%;
+  text-transform: uppercase;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  color: #38343f;
+}
 
-  .button:disabled {
-    cursor: not-allowed;
-    background-color: #D6D6D6;
-    border: 1px solid #D6D6D6;
-    color: #fff;
-  }
+.button:disabled {
+  cursor: not-allowed;
+  background-color: #d6d6d6;
+  border: 1px solid #d6d6d6;
+  color: #fff;
+}
 
-  .button--success {
-    background-color: #7AC016;
-    border: 1px solid #7AC016;
-    color: #fff;
-  }
+.button--success {
+  background-color: #7ac016;
+  border: 1px solid #7ac016;
+  color: #fff;
+}
 
-  .button--primary {
-    background-color: #22BBD2;
-    border: 1px solid #22BBD2;
-    color: #fff;
-  }
+.button--primary {
+  background-color: #22bbd2;
+  border: 1px solid #22bbd2;
+  color: #fff;
+}
 </style>

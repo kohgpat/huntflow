7758ef07945de89b99bffbed24311b85718a2d9c
Nikolay Burlov <kohgpat@gmail.com>
2018-09-06 03:33:58 +0800

    Fix modal position styles

--- src/components/ConfirmModal.vue
+++ src/components/ConfirmModal.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="modal">
     <div class="modal__container">
-      <header class="modal__header">
-        <slot name="title">
-          <h4 class="modal__title">Подтвердите ваше действие</h4>
-        </slot>
-      </header>
-      
-      <div class="modal__body">
-        <slot name="body">
-          <Button class="modal__control" @click="$emit('close')">Закрыть</Button>
-        </slot>
+      <div class="modal__content">
+        <header class="modal__header">
+          <slot name="title">
+            <h4 class="modal__title">Подтвердите ваше действие</h4>
+          </slot>
+        </header>
+        
+        <div class="modal__body">
+          <slot name="body">
+            <Button class="modal__control" @click="$emit('close')">Закрыть</Button>
+          </slot>
+        </div>
       </div>
     </div>
   </div>
@@ -50,33 +52,35 @@ export default {
 </script>
 
 <style scoped>
-  .modal {
-    position: relative;
-    width: 350px;
-    z-index: 1;
-  }
+.modal {
+  position: relative;
+}
 
-  .modal__container {
-    position: absolute;
-    background-color: #fff;
-    border-radius: 5px;
-    box-shadow: 0 0 15px rgba(0, 0, 0, 0.15);
-    width: 100%;
-  }
+.modal__container {
+  z-index: 1;
+}
 
-  .modal__header,
-  .modal__body {
-    display: flex;
-    align-items: center;
-    height: 50px;
-  }
+.modal__content {
+  position: absolute;
+  background-color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.15);
+  width: 350px;
+}
 
-  .modal__header {
-    border-bottom: 1px solid #EBEBEB;
-    padding: 14px 20px;
-  }
+.modal__header,
+.modal__body {
+  display: flex;
+  align-items: center;
+  height: 50px;
+}
 
-  .modal__body {
-    padding: 14px 20px;
-  }
+.modal__header {
+  border-bottom: 1px solid #ebebeb;
+  padding: 14px 20px;
+}
+
+.modal__body {
+  padding: 14px 20px;
+}
 </style>

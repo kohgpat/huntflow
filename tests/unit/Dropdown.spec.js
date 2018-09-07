bccaefc3e6f2724f349c6ecd45176edd7bd9357c
Nikolay Burlov <kohgpat@gmail.com>
2018-09-07 23:27:57 +0800

    Add Dropdown items rendering and items handlers invocations test

--- tests/unit/Dropdown.spec.js
+++ tests/unit/Dropdown.spec.js
@@ -12,4 +12,47 @@ describe("Dropdown", () => {
     expect(dropdown).toMatchSnapshot();
     expect(hiddenDropdown).toMatchSnapshot();
   });
+
+  it("should display items in dropdown", () => {
+    const editHandler = jest.fn();
+    const deleteHandler = jest.fn();
+
+    const items = [
+      {
+        name: "Edit",
+        button: true,
+        handler: () => {
+          editHandler();
+        }
+      },
+      {
+        name: "Delete",
+        button: true,
+        handler: () => {
+          deleteHandler();
+        }
+      }
+    ];
+
+    const dropdown = shallowMount(Dropdown, {
+      propsData: {
+        items
+      }
+    });
+    expect(dropdown.findAll(".dropdown__item").length).toEqual(2);
+
+    dropdown
+      .findAll(".dropdown__item")
+      .at(0)
+      .find("button")
+      .trigger("click");
+    dropdown
+      .findAll(".dropdown__item")
+      .at(1)
+      .find("button")
+      .trigger("click");
+
+    expect(editHandler).toBeCalled();
+    expect(deleteHandler).toBeCalled();
+  });
 });
